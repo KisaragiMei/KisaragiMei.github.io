05d23c8090d9f0080246ab18a6fca6fbc3b57288
KisaragiMei <296540606+KisaragiMei@users.noreply.github.com>
2026-06-30 21:44:01 +0700

    Site updated: 2026-06-30 21:44:01

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1782829595730";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1782830634721";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
